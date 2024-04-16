b933db4b8a025db2902c29d669127bdee506d233
Lucy <souzalucyg@gmail.com>
2024-04-15 21:47:39 -0300

    arrumando o 00

--- exercicios/para-casa/exercicio_farmacia.py
+++ exercicios/para-casa/exercicio_farmacia.py
@@ -12,18 +12,18 @@ Farmacia Popular está em promoção. A cada 2 produtos iguais comprados(foque s
 # desenvolva a logica condicional
 if primeiro_produto == segundo_produto:
     print("Desconto de 5 reais")
-    if primeiro_produto == 0053 and segundo_produto == 0053:
+    if primeiro_produto == 53 and segundo_produto == 53:
         print("Total de 4.6+4.6-5= 4.4 reais")
-    elif primeiro_produto == 0045 and segundo_produto == 0045:
+    elif primeiro_produto == 45 and segundo_produto == 45:
         print("Total de 50+50-5= 95 reais")
 else:
-    if primeiro_produto == 0054 or segundo_produto == 0054:
+    if primeiro_produto == 54 or segundo_produto == 54:
         print("Desconto de 50%")
-        if primeiro_produto == 0054 and segundo_produto == 0054:
+        if primeiro_produto == 54 and segundo_produto == 54:
             print("Total de (10.4+10.4)*50/100 = 10.4 reais")
-        elif primeiro_produto == 0054 and segundo_produto == 0053:
+        elif primeiro_produto == 54 and segundo_produto == 53:
             print("Total de (10.4+4.6)*50/100 = 7.5 reais")
-        elif primeiro_produto == 0054 and segundo_produto == 0045:
+        elif primeiro_produto == 54 and segundo_produto == 45:
             print("Total de (10.4+50)*50/100 = 30.2 reais")
     else:
         print("Não recebe nenhum desconto")
